7cb5348dea283e953fdc73ea49dbe98d64cee496
ovi <kubikas.3@gmail.com>
2022-04-04 03:19:38 +0300

    [new] Table tests

---
+++ src/containers/ItemTable.test.tsx
@@ -0,0 +1,76 @@
+import { render, screen } from "@testing-library/react";
+import { Formik } from "formik";
+import { ItemTable } from "./ItemTable";
+import { HikeInfo } from "../types";
+
+test("on too much weight, alert should be rendered", () => {
+  const initialValues: HikeInfo = {
+    capacity: 15,
+    distance: 1000,
+    season: "spring",
+  };
+
+  render(
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      <ItemTable />
+    </Formik>
+  );
+
+  expect(
+    screen.getByText(
+      /total weight exceeds backpack capacity — get a larger backpack or reduce distance/i
+    )
+  ).toBeInTheDocument();
+});
+
+test("on positive distance, items to be rendered", () => {
+  const initialValues: HikeInfo = {
+    capacity: 15,
+    distance: 10,
+    season: "spring",
+  };
+
+  render(
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      <ItemTable />
+    </Formik>
+  );
+
+  expect(screen.getAllByRole("row").length).toBeGreaterThan(0);
+});
+
+test("on negative distance, no items to be rendered", () => {
+  const initialValues: HikeInfo = {
+    capacity: 15,
+    distance: -1,
+    season: "spring",
+  };
+
+  render(
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      <ItemTable />
+    </Formik>
+  );
+
+  expect(screen.getAllByRole("row")).toHaveLength(2);
+});
+
+test("on summer, cap to be rendered", () => {
+  const initialValues: HikeInfo = {
+    capacity: 15,
+    distance: 10,
+    season: "summer",
+  };
+
+  render(
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      <ItemTable />
+    </Formik>
+  );
+
+  const capRow = screen
+    .getAllByRole("row")
+    .find((r) => r.textContent?.startsWith("Cap"));
+
+  expect(capRow).toBeInTheDocument();
+});
